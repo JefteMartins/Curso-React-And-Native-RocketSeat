1af7b5528d4a80cb9251ab72be979aab664addd4
Jefte Martins <jefte.martins@hotmail.com>
2023-03-08 19:45:42 -0300

    adding logo image

--- 01-fundamentos-react.js/src/components/Header.jsx
+++ 01-fundamentos-react.js/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import styles from './Header.module.css'
+import ingiteLogo from '../assets/ignite-logo.svg'
 
 export function Header () {
     return (
         <header className={styles.header}>
-            <strong> Header </strong>
+            <img src={ingiteLogo} alt="Ignite" />
         </header>
     )
 }
