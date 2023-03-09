7c93ccf89fa11232b1b539bab6cc9407d475e59e
Jefte Martins <jefte.martins@hotmail.com>
2023-03-09 17:51:46 -0300

    Finalizando sidebar

--- 01-fundamentos-react.js/src/App.jsx
+++ 01-fundamentos-react.js/src/App.jsx
@@ -1,22 +1,23 @@
-import { Header } from "./components/Header"
-import { Post } from "./Post"
-import "./global.css"
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
-
   return (
-      <>
-        <Header/>
-        <Post
-        author="Jefte"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-        />
-        <Post
-        author="Martins"
-        content="Novo texto"
-        />
-      </>
-  )
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar /> 
+        <main>
+          <Post
+            author="Jefte"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          />
+          <Post author="Martins" content="Novo texto" />
+        </main>
+      </div>
+    </>
+  );
 }
-
-
